46cfd03c5be84c5fec68c8af305f289a6c55573f
Vitaly <ivanov.arduan@yandex.ru>
2022-01-10 09:43:31 +0300

    Пытаюсь встроить графики через функцию, но ничего не получается.

--- myblog/views.py
+++ myblog/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from .forms import form_model
 
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 def show(request):
     titles = Post.objects.order_by('title')
     return render(request, 'myblog/example.html', {
@@ -26,3 +29,21 @@ def get_name(request):
 def about(request):
     form = form_model(request.POST)
     return render(request, 'myblog/about.html', {'form': form})
+
+
+def graph(request):
+    loss = np.array([1, 0.95, 0.92, 0.89, 0.83, 0.76, 0.70, 0.63, 0.54, 0.48])
+    epochs = np.array(list(range(10)))
+    plt.plot(loss, epochs, label="Loss Curve 1", linestyle="dashed", marker='*', color='red')
+    # Plot 1
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    # Plot 2
+    plt.title("Loss - Epoch Curve")
+    # Plot 3
+    plt.grid("on")
+    # Plot 4
+    plt.legend()
+    # Plot 5
+    plt.show()
+    return render('myblog/about.html', {'graph': graph})
